b529d759dd06c2b2fc138d57a1756777adfd6c91
Karina Kupryianava <karina.kupryianava@gmail.com>
2025-11-02 18:54:44 +0300

    init: create services

---
+++ academics-service/src/test/java/com/brsm/academicsservice/AcademicsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brsm.academicsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
